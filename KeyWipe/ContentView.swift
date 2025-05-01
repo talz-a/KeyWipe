d932c984ebf8ea5a1045a0d75fac0a7a9d080bea
Talal <talal.ahm25@gmail.com>
2025-05-01 16:20:23 -0400

    Initial Commit

---
+++ KeyWipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeyWipe
+//
+//  Created by Talal on 2025-05-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeyWipe/KeyWipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyWipeApp.swift
+//  KeyWipe
+//
+//  Created by Talal on 2025-05-01.
+//
+
+import SwiftUI
+
+@main
+struct KeyWipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
